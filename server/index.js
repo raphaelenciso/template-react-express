99165584a7fee58fe272763277c2699c92a23002
shineljin <psyruz18@gmail.com>
2023-06-08 15:33:16 +0800

    fix render

--- server/index.js
+++ server/index.js
@@ -1,17 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import router from "./router/router.js";
+
 dotenv.config();
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(express.json({ limit: "5mb", extended: true }));
 app.use(express.urlencoded({ limit: "5mb", extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser());
 
 app.use("/api", router);
-
 app.get("/", (req, res) => {
   res.send("Server is running");
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 
 let port = process.env.PORT || 4000;
